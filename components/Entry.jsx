9676c3295c1f37a08ac179690dc1413b3046cf62
egbetayo <damilola.egbetayo@stanbicibtc.com>
2023-09-21 00:12:53 +0100

    feat: fixed the stylings and overflows

--- components/Entry.jsx
+++ components/Entry.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+/* eslint-disable react/prop-types */
 import "./entry/Entry.css";
 
 const Entry = (props) => {
   return (
-    <div className="containers">
-      <div className="con">
+    <div className="entryContainer">
+      <div className="entrySubContainer">
         <h2 className="headings">{props.emoji}</h2>
         <h2 className="name">{props.name}</h2>
         <p className="meaning">{props.meaning}</p>
